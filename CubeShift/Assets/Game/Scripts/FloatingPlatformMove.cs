536a400b952dd5c3ed6433053ba3c940315a8868
MatthewBanathy <129894076+MatthewBanathy@users.noreply.github.com>
2023-04-05 20:39:52 -0700

    Updated OtherScene and created FloatingPlatform, script included

---
+++ CubeShift/Assets/Game/Scripts/FloatingPlatformMove.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingPlatformMove : MonoBehaviour
+{
+    public GameObject leftPoint;
+    public GameObject rightPoint;
+    private Vector3 leftPos;
+    private Vector3 rightPos;
+    public int speed;
+    public bool goingLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftPos = leftPoint.transform.position;
+        rightPos = rightPoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //Make the platform move from one place to the other
+    private void Move()
+    {
+        if (goingLeft)
+        {
+            if (transform.position.x <= leftPos.x)
+            {
+                //Then we are not going left
+                goingLeft = false;
+            }
+            else
+            {
+                //Go left instead
+                transform.position += Vector3.left * Time.deltaTime * speed;
+            }
+        }
+        else
+        {
+            if (transform.position.x >= rightPos.x)
+            {
+                //Then we're going left
+                goingLeft = true;
+            }
+            else
+            {
+                //Go right instead
+                transform.position += Vector3.right * Time.deltaTime * speed;
+            }
+        }
+    }
+}
