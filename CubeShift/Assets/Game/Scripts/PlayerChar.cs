ee7dd81c7aa85dffe4b0399a3d02af382d9ee761
MartinAVP <mv407692@gmail.com>
2023-04-05 22:51:53 -0700

    added a gate and organized the testScene
    Made folders with the testScene
    and added a gate prefab

--- CubeShift/Assets/Game/Scripts/PlayerChar.cs
+++ CubeShift/Assets/Game/Scripts/PlayerChar.cs
@@ -6,6 +6,7 @@ public class PlayerChar : MonoBehaviour
 {
 
     private Vector3 spawnPoint;
+    public int goldenKey;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,8 +30,23 @@ public class PlayerChar : MonoBehaviour
         {
             Respawn();
         }
-        else if (other.gameObject.tag == "key")
+        else if(other.gameObject.tag == "goldGate") 
         {
+            print("golden gate");
+            if(goldenKey == 1) 
+            {
+                other.transform.parent.gameObject.SetActive(false);
+                //other.gameObject.SetActive(false);
+            }
+            else
+            {
+                print("you don't have a key");
+            }
+        }
+        else if (other.gameObject.tag == "goldKey")
+        {
+            goldenKey++;
+            other.gameObject.SetActive(false);
             print("key");
         }
     }
