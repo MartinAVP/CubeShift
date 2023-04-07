a15d258d0fabe0dcab10ffa425f222893fb5cbb4
MartinAVP <mv407692@gmail.com>
2023-04-07 01:16:57 -0700

    Fixed the platform
    Not yet a prefab

--- CubeShift/Assets/Game/Scripts/FloatingPlatformMove.cs
+++ CubeShift/Assets/Game/Scripts/FloatingPlatformMove.cs
@@ -19,50 +19,104 @@ public class FloatingPlatformMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        leftPos = leftPoint.transform.position;
-        rightPos = rightPoint.transform.position;
+        //leftPos = leftPoint.transform.position;
+        //rightPos = rightPoint.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Move();
+        movement();
+        //Move();
     }
 
-    private void checkPoints()
+    private void movement()
     {
-
-    }
-
-
-    //Make the platform move from one place to the other
-    private void Move()
-    {
-        if (goingLeft)
+        if(goingLeft == false) 
         {
-            if (transform.position.x <= leftPos.x)
+            if (gameObject.transform.position == leftPoint.transform.position)
             {
-                //Then we are not going left
-                goingLeft = false;
-            }
-            else
-            {
-                //Go left instead
-                transform.position += Vector3.left * Time.deltaTime * speed;
-            }
-        }
-        else
-        {
-            if (transform.position.x >= rightPos.x)
-            {
-                //Then we're going left
+                print("toco el punto izquierdo");
                 goingLeft = true;
             }
             else
             {
-                //Go right instead
-                transform.position += Vector3.right * Time.deltaTime * speed;
+                transform.position = Vector3.MoveTowards(transform.position, leftPoint.transform.position, speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (gameObject.transform.position == rightPoint.transform.position)
+            {
+                print("toco el punto derecho");
+                goingLeft = false;
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, rightPoint.transform.position, speed * Time.deltaTime);
             }
         }
     }
+
+    //Make the platform move from one place to the other
+    private void Move()
+    {
+        if (goingLeft == false)
+        {
+            if (transform.position.x == rightPoint.transform.position.x && transform.position.y == rightPoint.transform.position.y)
+            {
+                //Then we are not going left
+                print("leftPoint");
+                goingLeft = true;
+            }
+            else
+            {
+                //Go left instead
+                //transform.position += Vector3.left * Time.deltaTime * speed;
+                transform.position = Vector3.MoveTowards(transform.position, leftPoint.transform.position, speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (transform.position == leftPoint.transform.position)
+            {
+                //Then we're going left
+                goingLeft = false;
+                print("touched position");
+            }
+            else
+            {
+                //Go right instead
+                //transform.position += Vector3.right * Time.deltaTime * speed;
+                transform.position = Vector3.MoveTowards(transform.position, rightPoint.transform.position, speed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //print("player in range stayed");
+            other.transform.position = Vector3.MoveTowards(other.transform.position, transform.position, 1.3f * speed * Time.deltaTime);
+        }
+    }
+
+    /*
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                print("player collided");
+                if(goingLeft) 
+                {
+                    collision.transform.position = Vector3.MoveTowards(collision.transform.position, leftPoint.transform.position, speed * Time.deltaTime);
+                }
+                else
+                {
+                    collision.transform.position = Vector3.MoveTowards(collision.transform.position, leftPoint.transform.position, speed * Time.deltaTime);
+                }
+            }
+        }
+    */
 }
