277a734531093ee5848e3553aa8ca11af7cdb2e2
MartinAVP <mv407692@gmail.com>
2023-04-11 20:21:10 -0700

    Matthew Level Cabled

--- CubeShift/Assets/Game/Scripts/UIController.cs
+++ CubeShift/Assets/Game/Scripts/UIController.cs
@@ -13,7 +13,7 @@ public class UIController : MonoBehaviour
     public string levelDescription;
     private Animator animatore;
 
-    public int deathMsgId;
+    private int deathMsgId;
     // Start is called before the first frame update
     void Start()
     {
